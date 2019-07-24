982907f0c18057279715237b26cea82604a1c3b6
Tori Fortune <toriolley@gmail.com>
2019-07-23 21:42:08 -0500

    fixed connection url error in server.js file

--- routes/api/books.js
+++ routes/api/books.js
@@ -1,9 +1,28 @@
+// const router = require("express").Router();
+// const bookController = require("../../controllers/bookController");
+
+// // Matches with "/api/books"
+// router.route("/")
+//   .get(bookController.findBooks)
+//   .post(bookController.addBook);
+
+// // Matches with "/api/books/:id"
+// router
+//   .route("/:id")
+//   // .get(bookController.findById)
+//   // .put(bookController.update)
+//   .delete(bookController.remove);
+
+// module.exports = router;
+
+
+
 const router = require("express").Router();
 const bookController = require("../../controllers/bookController");
 
 // Matches with "/api/books"
 router.route("/")
-  .get(bookController.findBooks)
+  .get(bookController.findBook)
   .post(bookController.addBook);
 
 // Matches with "/api/books/:id"
@@ -11,6 +30,6 @@ router
   .route("/:id")
   // .get(bookController.findById)
   // .put(bookController.update)
-  .delete(bookController.remove);
+  .delete(bookController.removeBook);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,24 +1,65 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const mongoose = require("mongoose");
+const dotenv =require("dotenv");
+dotenv.config;
 const routes = require("./routes");
+const mongoose = require("mongoose");
+const express = require("express");
+// const bodyParser = require("body-parser");
+// const server = require('http').createServer(app);
+// const io = require('socket.io').listen(server);
+
 const app = express();
 const PORT = process.env.PORT || 6516;
 
-// Define middleware here
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-// Serve up static assets (usually on heroku)
+
+
+// io.on('connection', client => {
+//   client.on('notifyUser', () => {
+//     console.log('client is saving book to db ');
+//     client.emit('timer', new Date());
+//   });
+// });
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Static files for deployment
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
-// Add routes, both API and view
+
 app.use(routes);
 
-// Connect to the Mongo DB
-// mongoose.connect(process.env.MONGODB_URI || process.env.DB_URI, { useNewUrlParser: true });
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/bookworm");
-// Start the API server
-app.listen(PORT, function() {
-  console.log(`🌎  ==> You are now connected on PORT ${PORT}!`);
+
+app.listen(PORT, () => {
+  console.log(`API server listing on port ${PORT}!`);
 });
+
+
+
+
+
+
+
+
+
+
+
+
+// // Define middleware here
+// app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(bodyParser.json());
+// // Serve up static assets (usually on heroku)
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static("client/build"));
+// }
+// // Add routes, both API and view
+// app.use(routes);
+
+// // Connect to the Mongo DB
+// // mongoose.connect(process.env.MONGODB_URI || process.env.DB_URI, { useNewUrlParser: true });
+// mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/bookworm");
+// // Start the API server
+// app.listen(PORT, function() {
+//   console.log(`🌎  ==> You are now connected on PORT ${PORT}!`);
+// });
